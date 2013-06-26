bf602e91bcf4913c8bb9f6acd9f2a2661ada146f
Alex Kearney <Alex Kearney@192.168.1.110>
2013-06-26 15:30:08 -0600

    Issues with git, sorry for the delay. I have done multiple things since your message, including setting up a toy maze to play with. I need to think more about the slime mold itself.

---
+++ src/slimemoldmodel/SlimeMold.java
@@ -0,0 +1,48 @@
+// Grid For Slime Mold Model Simulation
+
+/* Written by Charlotte Lilley and Alex Kearney
+ *                  2013 Summer Research
+ * 
+ *   
+ * 
+ *   Alex's Change notes:
+ *      25-6-13:
+ *          - Initial Set-up
+ *          - My plan for the construction of the slime mold is to have multiple
+ *  actors in a particular setting which manage their current loaction --
+ *  like the nuclei -- which function as a whole through the 
+ * 
+ *   Charlotte's Change notes:
+ * 
+ * 
+ * _________________________________________________________________________
+ * 
+ *  Current Goals:
+ * 
+ *  - Override important actions ------------------------------------ [ ]
+ *  - Change image instantiation ------------------------------------ [ ]
+ *  - Add formulae for actions -------------------------------------- [ ]
+ *  - Multiple actors on grid --------------------------------------- [ ]
+ * 
+ */
+
+package slimemoldmodel;
+
+import info.gridworld.actor.Actor;
+import java.math.MathContext;
+
+public class SlimeMold extends Actor {
+
+    
+    
+    @Override
+    public void act() {
+        //Find state
+        
+        // update
+        
+        
+        //act
+    }
+      
+}
---
+++ src/slimemoldmodel/SlimeMoldModel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slimemoldmodel;
+
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.BoundedGrid;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+/**
+ *
+ * @author Alex Kearney
+ */
+public class SlimeMoldModel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+             int numberOfRows =5;
+        int numberOfColumns=5;
+        BoundedGrid boundedGrid;
+         World world;
+       
+        boundedGrid = new BoundedGrid(numberOfRows, numberOfColumns);//this makes the gui we use to do the grid stuff
+        world = new World(boundedGrid); // this is where we store the things which interact with the grid
+        world.show();//this makes the grid visible
+
+        /*
+         *  Basic Grid Plan:
+         *      s = Start
+         *      e = End
+         *      d = Dead End
+         *      R = Rock
+         * 
+         *            
+         *         A
+         *   0  1  2  3  4    (B , A)
+         *    -  -  -  -  - 
+         *  | s  R           | 0
+         *  |    R     R     | 1
+         *  |    R     R  d  | 2 B
+         *  |    R     R  R  | 3
+         *  |             e  | 4
+         *    -  -  -  -  - 
+         * 
+         * 
+         * 
+         *   Rocks: 
+         *      (0,1), (1,1), (2,1), (3,1), (4,1), (2,3), (3,3), (4,3)
+         * 
+         */
+ 
+        // --- Create The Rocks To Build Maze ---
+        
+        boundedGrid.put(new Location(0,1), new Rock());
+        boundedGrid.put(new Location(1,1), new Rock());
+        boundedGrid.put(new Location(2,1), new Rock());
+        boundedGrid.put(new Location(3,1), new Rock());
+        boundedGrid.put(new Location(2,3), new Rock());
+        boundedGrid.put(new Location(3,3), new Rock());
+        boundedGrid.put(new Location(3,4), new Rock());
+       boundedGrid.put(new Location(1,3), new Rock());
+    }
+}
