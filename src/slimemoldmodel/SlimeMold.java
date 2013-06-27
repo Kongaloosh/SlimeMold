4fda7be310972d826b30e17d3f846acfec3c16c0
Alex Kearney <Alex Kearney@192.168.1.110>
2013-06-27 13:52:02 -0600

    Slime Mold will generate it's own end points for management of self. Just call and give the connections.

--- src/slimemoldmodel/SlimeMold.java
+++ src/slimemoldmodel/SlimeMold.java
@@ -3,14 +3,29 @@
 /* Written by Charlotte Lilley and Alex Kearney
  *                  2013 Summer Research
  * 
- *   
+ *  Note For Next Person:
  * 
+ *      Hey C, I put this in so that you would have some sort of idea as to what
+ * I've been doing and what I'll do next. I've put change notes in before and
+ * have a section for you. When you finish reading this you can just delete it,
+ * that way I know you've got my message. Also, if you have any overarching
+ * questions, just put them here. If you want to know what a specific piece of 
+ * code is, just put "//" which makes a comment and then add something, like 
+ * 
+ * //WTF ALEX! EXPPPLAAAINNN!
+ *
  *   Alex's Change notes:
- *      25-6-13:
+ *      25-06-13:
  *          - Initial Set-up
  *          - My plan for the construction of the slime mold is to have multiple
  *  actors in a particular setting which manage their current loaction --
  *  like the nuclei -- which function as a whole through the 
+ * 
+ *      26-06-13:
+ *          - More slime mold work, but not much else
+ *      
+ *      27-06-13
+ *          - Fleshed out SlimeMold Class' structure.
  * 
  *   Charlotte's Change notes:
  * 
@@ -24,6 +39,7 @@
  *  - Add formulae for actions -------------------------------------- [ ]
  *  - Multiple actors on grid --------------------------------------- [ ]
  *  - method that grnerates the nucli and structure to build safely - [ ]
+ *      - Find a way to input the maze & connections ---------------- [ ]
  */
 
 package slimemoldmodel;
--- src/slimemoldmodel/SlimeMoldModel.java
+++ src/slimemoldmodel/SlimeMoldModel.java
@@ -1,6 +1,41 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+// Grid For Slime Mold Model Simulation
+
+/* Written by Charlotte Lilley and Alex Kearney
+ *                  2013 Summer Research
+ * 
+ *  
+ *  Note For Next Person:
+ * 
+ *      Hey C, I put this in so that you would have some sort of idea as to what
+ * I've been doing and what I'll do next. I've put change notes in before and
+ * have a section for you. When you finish reading this you can just delete it,
+ * that way I know you've got my message. Also, if you have any overarching
+ * questions, just put them here. If you want to know what a specific piece of 
+ * code is, just put "//" which makes a comment and then add something, like 
+ * 
+ * //WTF ALEX! EXPPPLAAAINNN!  
+ * 
+ * 
+ *   Alex's Change notes:
+ *      25-6-13:
+ *          - Initial Set-up
+ *          - My plan for the construction of the slime mold is to have multiple
+ *  actors in a particular setting which manage their current loaction --
+ *  like the nuclei -- which function as a whole through the 
+ * 
+ *   Charlotte's Change notes:
+ * 
+ * 
+ * _________________________________________________________________________
+ * 
+ *  Current Goals:
+ * 
+ *  - Build toy maze ------------------------------------------------ [X]
+ *  - Create SlimeMold Class ---------------------------------------- [X]
+ *  - Create SlimeMold Builder -------------------------------------- [X]
+ *      - Create actors for SlimeMold Endpoints --------------------- [ ]
+ *  - Multiple actors on grid --------------------------------------- [ ]
+ * 
  */
 package slimemoldmodel;
 
@@ -10,21 +45,15 @@ import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 import java.util.ArrayList;
 
-/**
- *
- * @author Alex Kearney
- */
 public class SlimeMoldModel {
 
-    /**
-     * @param args the command line arguments
-     */
+    ArrayList<SlimeMold> slimeMold;
+    
     public static void main(String[] args) {
-             int numberOfRows =5;
-        int numberOfColumns=5;
+        int numberOfRows = 5;
+        int numberOfColumns = 5;
         BoundedGrid boundedGrid;
-         World world;
-       
+        World world;
         boundedGrid = new BoundedGrid(numberOfRows, numberOfColumns);//this makes the gui we use to do the grid stuff
         world = new World(boundedGrid); // this is where we store the things which interact with the grid
         world.show();//this makes the grid visible
@@ -53,21 +82,24 @@ public class SlimeMoldModel {
          *      (0,1), (1,1), (2,1), (3,1), (4,1), (2,3), (3,3), (4,3)
          * 
          */
- 
+
         // --- Create The Rocks To Build Maze ---
-        
-        boundedGrid.put(new Location(0,1), new Rock());
-        boundedGrid.put(new Location(1,1), new Rock());
-        boundedGrid.put(new Location(2,1), new Rock());
-        boundedGrid.put(new Location(3,1), new Rock());
-        boundedGrid.put(new Location(2,3), new Rock());
-        boundedGrid.put(new Location(3,3), new Rock());
-        boundedGrid.put(new Location(3,4), new Rock());
-       boundedGrid.put(new Location(1,3), new Rock());
-    }
-  
-    public ArrayList<SlimeMold> generateSlime(){
-        return new ArrayList
+
+        boundedGrid.put(new Location(0, 1), new Rock());
+        boundedGrid.put(new Location(1, 1), new Rock());
+        boundedGrid.put(new Location(2, 1), new Rock());
+        boundedGrid.put(new Location(3, 1), new Rock());
+        boundedGrid.put(new Location(2, 3), new Rock());
+        boundedGrid.put(new Location(3, 3), new Rock());
+        boundedGrid.put(new Location(3, 4), new Rock());
+        boundedGrid.put(new Location(1, 3), new Rock());
     }
 
+    public void generateSlime(int numberOfPoints, ArrayList<Location> connections) {
+        for (int i = 0; i < connections.size(); i++) {
+            int startNode = connections.get(i).getCol();
+            int endNode = connections.get(i).getRow();
+            slimeMold.add(new SlimeMold(startNode , endNode));
+        }
+    }
 }
